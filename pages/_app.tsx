3af0a6ae31657576417c6716f3db2b024f67bf16
Rohit <RohitSinghEmail@protonmail.com>
2022-10-08 18:59:08 +0530

    initial frontend

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,35 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import "../styles/globals.css"
+import Head from "next/head"
+import type { AppProps } from "next/app"
+import { MoralisProvider } from "react-moralis"
+import { ApolloProvider, ApolloClient, InMemoryCache } from "@apollo/client"
+import Layout from "../components/Layout"
+import NetworkBanner from "../components/NetworkBanner"
+import { NotificationProvider } from "web3uikit"
+
+const client = new ApolloClient({
+  cache: new InMemoryCache(),
+  uri: process.env.NEXT_PUBLIC_SUBGRAPH_URL,
+})
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <title>NFT Marketplace</title>
+        <link rel="shortcut icon" href="/favicon.ico" />
+      </Head>
+      <MoralisProvider initializeOnMount={false}>
+        <ApolloProvider client={client}>
+          <NotificationProvider>
+            <NetworkBanner />
+            <Layout>
+              <Component {...pageProps} />
+            </Layout>
+          </NotificationProvider>
+        </ApolloProvider>
+      </MoralisProvider>
+    </>
+  )
 }
-
 export default MyApp
